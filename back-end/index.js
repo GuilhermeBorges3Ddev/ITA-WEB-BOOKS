e42ca1bd04323722c8aa9169366f8b9a263de599
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-11-23 04:35:16 -0300

    Arquivo index.js inicializador da API  Node.JS funcionando

--- back-end/index.js
+++ back-end/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 3000; //porta padrão
+const port = 3000; //porta padrão para a API Node.JS
 const mysql = require('mysql');
 
 //configurando o body parser para pegar POSTS mais tarde
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+//definindo as rotas
+const router = express.Router();
+router.get('/', (req, res) => res.json(
+    {
+        message: 'GET no diretório raiz da livraria funcionando!'
+    }
+));
+app.use('/', router);
+
+//inicia o servidor
+app.listen(port);
+console.log('API funcionando!');

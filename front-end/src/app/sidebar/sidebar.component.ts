29becbc92f905b9bf2de2b57740b7160d4a7a919
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-11-25 05:22:44 -0300

    sidebar TS e HTML montados

--- front-end/src/app/sidebar/sidebar.component.ts
+++ front-end/src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+//Comunicando nosso front com o arquivo de backend "server.ts via: "
+//sidebar.component.ts -----> app.module.ts
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -7,9 +11,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  sidelinks = this.http.get<any[]>('http://localhost:4201/listar/:CategoryID?');
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get("http://localhost:4201/listar/:CategoryID?")
   }
 
 }

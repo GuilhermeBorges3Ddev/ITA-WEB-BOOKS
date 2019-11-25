27a7df9b8e2b607e87ce02c75c61c17844ef567c
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-11-25 02:09:49 -0300

    Front minimamente integrado com o backend

--- front-end/server/server.ts
+++ front-end/server/server.ts
@@ -1,1 +1,23 @@
-console.log("Funfante");
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+    if ('OPTIONS' == req.method) {
+        res.sendStatus(200);
+    } else {
+        console.log(`${req.ip} ${req.method} ${req.url}`);
+        next();
+    }
+})
+
+app.get('/', (req, res) => {
+    res.send([{ message: 'ola angular' }]);
+})
+
+app.listen(4201, 'localhost', function () {
+    console.log("Server routes are running on 4201");
+});

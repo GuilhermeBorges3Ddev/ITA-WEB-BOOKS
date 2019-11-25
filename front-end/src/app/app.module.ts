1a4b3d26c3f035d2ec5bef189999b07891f5318a
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-11-25 05:15:45 -0300

    sidebar pre montada

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 
 //Comunicando nosso front com o arquivo de backend "server.ts"
 import { HttpClientModule } from '@angular/common/http';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
